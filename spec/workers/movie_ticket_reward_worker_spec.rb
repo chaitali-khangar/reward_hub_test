c62972e5dcdeab54c60fdebb1414f959fdf87f77
chaitali <chaitali.khangar@gmail.com>
2025-05-14 22:31:16 +0530

    space added

--- spec/workers/movie_ticket_reward_worker_spec.rb
+++ spec/workers/movie_ticket_reward_worker_spec.rb
@@ -15,18 +15,21 @@ RSpec.describe MovieTicketRewardWorker, type: :worker do
     it 'grants the movie ticket if spending exceeds $1000 within 60 days of first transaction' do
       FactoryBot.create(:transaction, user: user_eligible, amount: 1100, created_at: Time.zone.today - 30.days)
       expect(Rewards::MovieTicketRewardService).to receive(:new).with(user: user_eligible).and_call_original
+
       MovieTicketRewardWorker.new.perform
     end
 
     it 'does not grant the reward if spending is below $1000' do
       FactoryBot.create(:transaction, user: user_not_eligible, amount: 900, created_at: Time.zone.today - 30.days)
       expect(Rewards::MovieTicketRewardService).not_to receive(:new).with(user: user_not_eligible)
+
       MovieTicketRewardWorker.new.perform
     end
 
     it 'does not grant the reward if the first transaction was more than 60 days ago' do
       FactoryBot.create(:transaction, user: user_eligible, amount: 1500, created_at: Time.zone.today - 70.days)
       expect(Rewards::MovieTicketRewardService).not_to receive(:new).with(user: user_eligible)
+
       MovieTicketRewardWorker.new.perform
     end
 
@@ -39,6 +42,7 @@ RSpec.describe MovieTicketRewardWorker, type: :worker do
     it 'executes the job from the queue' do
       MovieTicketRewardWorker.perform_async
       expect(MovieTicketRewardWorker.jobs.size).to eq(1)
+
       MovieTicketRewardWorker.drain
       expect(MovieTicketRewardWorker.jobs.size).to eq(0)
     end
